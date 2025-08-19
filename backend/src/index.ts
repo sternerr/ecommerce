c3a1656621245af51be77ed7aa1d85a40e4a63d9
Noah <volpy2000@gmail.com>
2025-08-19 20:05:08 +0200

    feat(backend): express setup

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,12 @@
-console.log("test");
+import express from "express";
+import type { Express, Request, Response } from "express";
+
+const app: Express = express();
+
+app.get("/", (req: Request, res: Response) => {
+	res.json({ message: "Welcome to ecommerce backend" });
+});
+
+app.listen(3000, () => {
+	console.log(`Ecommerce backend @ http://localhost:3000`);
+})
