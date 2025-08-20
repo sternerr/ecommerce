b7a9e8983950eee2363a95da4735219853372901
Noah <volpy2000@gmail.com>
2025-08-20 14:14:20 +0200

    feat(backend): add env

---
+++ backend/src/config/env.ts
@@ -0,0 +1,14 @@
+import { config } from "dotenv";
+
+config({ path: `.env.${process.env.NODE_ENV || "development"}.local` });
+
+function getEnv(key: string, required = true): string {
+	const value = process.env[key];
+	if (!value && required) {
+		throw new Error(`Missing environemnt variable: ${key}`);
+	}
+
+	return value as string
+}
+
+export const PORT: number = parseInt(getEnv("PORT", false)) || 3000
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,12 +1,14 @@
 import express from "express";
 import type { Express, Request, Response } from "express";
 
+import { PORT } from "./config/env.ts";
+
 const app: Express = express();
 
 app.get("/", (req: Request, res: Response) => {
 	res.json({ message: "Welcome to ecommerce backend" });
 });
 
-app.listen(3000, () => {
-	console.log(`Ecommerce backend @ http://localhost:3000`);
+app.listen(PORT, () => {
+	console.log(`Ecommerce backend @ http://localhost:${PORT}`);
 })
