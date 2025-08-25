b48a44eae2bd21ca4bad792d0d4bbcee9409e0bb
Noah <volpy2000@gmail.com>
2025-08-25 14:13:45 +0200

    fix(backend): auth middleware import

--- backend/src/routes/order.route.ts
+++ backend/src/routes/order.route.ts
@@ -1,14 +1,29 @@
 import { Router } from "express";
 import type { Request, Response } from "express";
-import { signIn, signUp } from "../controllers/auth.controller.ts";
 import { OrderController } from "../controllers/order.controller.ts";
-import { authorize, isAdmin } from "../middleware/authorization.middleware.ts";
+import AuthoMiddleware from "../middleware/authorization.middleware.ts";
 
 const orderRouter: Router = Router();
 
-orderRouter.get("/", authorize, isAdmin, OrderController.getOrders);
-orderRouter.get("/users/:id", authorize, OrderController.getOrders);
-orderRouter.post("/", authorize, OrderController.createOrder);
-orderRouter.get("/:id", authorize, OrderController.getOrder);
+orderRouter.get("/",
+	AuthoMiddleware.authorize,
+	AuthoMiddleware.isAdmin,
+	OrderController.getOrders
+);
+
+orderRouter.get("/users/:id",
+	AuthoMiddleware.authorize,
+	OrderController.getOrders
+);
+
+orderRouter.post("/",
+	AuthoMiddleware.authorize,
+	OrderController.createOrder
+);
+
+orderRouter.get("/:id",
+	AuthoMiddleware.authorize,
+	OrderController.getOrder
+);
 
 export default orderRouter;
